5c4680cb48a4cf5ab611ca96d217023dfb1e77c4
Ray <chenxiaolei.ray@gmail.com>
2017-03-04 19:58:27 +0800

    基于注解配置Spring MVC

--- SpringMVCTest/src/org/fkit/controller/HelloController.java
+++ SpringMVCTest/src/org/fkit/controller/HelloController.java
@@ -3,8 +3,10 @@ package org.fkit.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,20 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by Ray on 2017/3/4.
  */
-public class HelloController implements Controller {
+
+//基于注解的控制器，可以同时处理多个请求动作，并且无需实现任何接口
+@Controller
+public class HelloController {
 
     private static final Log logger = LogFactory.getLog(HelloController.class);
 
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        logger.info("handleRequest 被调用");
-        ModelAndView mv = new ModelAndView();
+    @RequestMapping(value = "/hello")
+    public ModelAndView hello(){
+        logger.info("hello 方法被调用");
 
-        //这个Message要与jsp页面的Message对应
+        ModelAndView mv = new ModelAndView();
         mv.addObject("Message","HelloWorld");
         mv.setViewName("/WEB-INF/content/welcome.jsp");
 
         return mv;
-
     }
 }
